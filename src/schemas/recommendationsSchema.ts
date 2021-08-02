0819aa2b2685befbee8a7e51930be23e0c573010
Guga <contato.gustavobarbosa@outlook.com>
2021-08-02 16:00:58 -0300

    Preparando revisão do código do Eduardo

--- src/schemas/recommendationsSchema.ts
+++
@@ -1,9 +0,0 @@
-  
-import joi from 'joi';
-
-const recommendationsSchema = joi.object({
-  name: joi.string().min(3).required(),
-  youtubeLink: joi.string().pattern(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/).required()
-});
-
-export { recommendationsSchema };
